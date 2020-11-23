32618664995e01345861229b97f7e2ea62dbe6ab
Aurelien Brabant <aurelien@aurelienbrabant.fr>
2020-11-23 19:53:12 +0100

    Fixed ft_ulllenb norme error

--- src/intconv/ft_ulllenb.c
+++ src/intconv/ft_ulllenb.c
@@ -6,7 +6,7 @@
 /*   By: abrabant <abrabant@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/23 19:20:40 by abrabant          #+#    #+#             */
-/*   Updated: 2020/11/23 19:29:53 by abrabant         ###   ########.fr       */
+/*   Updated: 2020/11/23 19:51:53 by abrabant         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 size_t	ft_ulllenb(unsigned long long n, t_byte base)
 {
-	size_t	 c;
+	size_t	c;
 
 	c = 1;
 	while (n /= base)
