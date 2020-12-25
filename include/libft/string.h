d5cf81753c1739edf56b1586079875914f835b9f
abrabant <42@aurelienbrabant.fr>
2020-12-26 00:11:10 +0100

    Fix prototypes error

--- include/libft/string.h
+++ include/libft/string.h
@@ -6,7 +6,7 @@
 /*   By: abrabant <abrabant@42.student.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/07 14:17:27 by abrabant          #+#    #+#             */
-/*   Updated: 2020/12/24 22:12:55 by abrabant         ###   ########.fr       */
+/*   Updated: 2020/12/26 00:07:27 by abrabant         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ void	ft_bzero(void *s, size_t n);
 void	*ft_memcpy(void *dest, const void *src, size_t n);
 void	*ft_memccpy(void *dest, const void *src, int c, size_t n);
 void	*ft_memmove(void *dest, const void *src, size_t n);
-void	ft_memchr(const void *s, int c, size_t n);
+void	*ft_memchr(const void *s, int c, size_t n);
 int		ft_memcmp(const void *s1, const void *s2, size_t n);
 
 size_t	ft_strlen(const char *s);
@@ -45,7 +45,7 @@ char	*ft_strrev(char *str);
 char	*ft_strupr(char *str);
 char	*ft_strlwr(char *str);
 int		ft_strchri(const char *str, const char c);
-bool	t_strhsprfx(const char *s, const char *prfx);
+bool	ft_strhsprfx(const char *s, const char *prfx);
 bool	ft_strhssfx(const char *s, const char *sfx);
 
 #endif
