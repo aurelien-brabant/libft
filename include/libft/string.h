0466c7499b6f75a9243b8b6bcaf082992c85c440
abrabant <42@aurelienbrabant.fr>
2021-01-04 22:56:52 +0100

    Add ft_strtrim_uniq

--- include/libft/string.h
+++ include/libft/string.h
@@ -6,7 +6,7 @@
 /*   By: abrabant <abrabant@42.student.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/07 14:17:27 by abrabant          #+#    #+#             */
-/*   Updated: 2021/01/04 12:10:37 by abrabant         ###   ########.fr       */
+/*   Updated: 2021/01/04 22:10:09 by abrabant         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,6 +38,7 @@ char	*ft_strndup(const char *s, size_t n);
 char	*ft_substr(char const *s, unsigned int start, size_t len);
 char	*ft_strjoin(char const *s1, char const *s2);
 char	*ft_strtrim(char const *s1, char const *set);
+char	*ft_strtrim_uniq(const char *s, const char *set);
 char	**ft_split(char const *s, char c);
 char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
 
