179880ebb2f225b6b29e1e886e6eec81fbb7e43e
abrabant <abrabant@student.42.fr>
2021-02-22 03:10:47 +0100

    Remove calloc int overflow limitation

--- src/core/ft_calloc.c
+++ src/core/ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: abrabant <abrabant@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/08/31 21:49:57 by abrabant          #+#    #+#             */
-/*   Updated: 2020/11/08 13:05:19 by abrabant         ###   ########.fr       */
+/*   Updated: 2021/02/22 03:10:22 by abrabant         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,12 +16,6 @@
 
 /*
 ** libft implementation of calloc(3)
-**
-** The error handling is the same as the Linux implementation: if nmemb * size
-** overflows an int datatype, NULL is returned.
-**
-** The case where nmemb or size is 0 is not handled specifically because
-** malloc(3) should already do it.
 */
 
 void	*ft_calloc(size_t nmemb, size_t size)
@@ -30,8 +24,6 @@ void	*ft_calloc(size_t nmemb, size_t size)
 	size_t	i;
 
 	i = 0;
-	if (nmemb * size > UINT_MAX)
-		return (NULL);
 	mem = malloc(size * nmemb);
 	if (!mem)
 		return (NULL);
