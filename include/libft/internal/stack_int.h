ea3a427fe265764625ef97d2dcf2f899dd03926a
abrabant <abrabant@student.42.fr>
2021-03-26 21:07:41 +0100

    Fix stack header norm issue

--- include/libft/internal/stack_int.h
+++ include/libft/internal/stack_int.h
@@ -6,14 +6,14 @@
 /*   By: abrabant <abrabant@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/26 16:02:50 by abrabant          #+#    #+#             */
-/*   Updated: 2021/03/26 16:46:11 by abrabant         ###   ########.fr       */
+/*   Updated: 2021/03/26 21:07:19 by abrabant         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef STACK_INT_H
 # define STACK_INT_H
 
-#include "libft/array.h"
+# include "libft/array.h"
 
 typedef struct s_stack_int
 {
