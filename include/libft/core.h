28786311a8599f19e96e7164eb4f3edc8be22ba4
abrabant <abrabant@student.42.fr>
2021-03-31 15:11:35 +0200

    Add ft_selection_sort algorithm

--- include/libft/core.h
+++ include/libft/core.h
@@ -6,7 +6,7 @@
 /*   By: abrabant <abrabant@42.student.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/07 14:30:22 by abrabant          #+#    #+#             */
-/*   Updated: 2021/03/31 00:46:59 by abrabant         ###   ########.fr       */
+/*   Updated: 2021/03/31 14:33:18 by abrabant         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,8 @@ void		*ft_realloc(void *ptr, size_t osize, size_t nsize);
 void		ft_bubble_sort(void *mem, size_t nb_el, size_t size_el,
 				int (*cmp)(void *, void *));
 void		ft_insertion_sort(void *mem, size_t nb_el, size_t size_el,
+				int (*cmp)(void *, void *));
+void		ft_selection_sort(void *mem, size_t nb_el, size_t size_el,
 				int (*cmp)(void *, void *));
 
 long long	ft_clamp(long long nb, long long min, long long max);
